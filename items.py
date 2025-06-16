e7e8ff31d6cc5012b68069b95068ce007f76ffe2
jemina16 <ella.lehman@helsinki.fi>
2025-06-16 13:41:17 +0300

    Add images to recipes

--- items.py
+++ items.py
@@ -31,7 +31,19 @@ def get_comments(item_id):
              FROM comments, users
              WHERE comments.item_id = ? AND comments.user_id = users.id
              ORDER BY comments.id DESC"""
+    
+def get_images(item_id):
+    sql = "SELECT id FROM images WHERE item_id = ?"
     return db.query(sql, [item_id])
+
+def add_image(item_id, image):
+    sql = "INSERT INTO images (item_id, image) VALUES (?, ?)"
+    db.execute(sql, [item_id, image])
+
+def get_image(image_id):
+    sql = "SELECT image FROM images WHERE id = ?"
+    result = db.query(sql, [image_id])
+    return result[0][0] if result else None
 
 def get_classes(item_id):
     sql = "SELECT title, value FROM item_classes WHERE item_id = ?"
